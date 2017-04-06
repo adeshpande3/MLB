254949087ba348709b53d848f7645ee5911e95e3
Adit Deshpande <adeshpande3@ucla.edu>
2017-04-06 12:15:27 -0700

    Added highlight effect when team is clicked on

--- src/Team.js
+++ src/Team.js
@@ -7,11 +7,35 @@ export default class Team extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.highlightImage = this.highlightImage.bind(this);
+    this.unhighlightImage = this.unhighlightImage.bind(this);
     this.showAnimation = this.showAnimation.bind(this);
     this.state = {text: '',
 				  numberOfClicks: 0,
 				  teamData: '',
-				  animation: ''}
+				  animation: '',
+				  image: ''}
+  }
+
+  componentDidMount() {
+  	this.setState({image: 
+  		<div className = "TeamPicture">
+		    <img src={this.props.url} alt={this.props.teamName} onClick={this.handleClick}/>
+		</div>})
+  }
+
+  highlightImage(){
+    this.setState({image: 
+  		<div className = "TeamPictureHighlighted">
+		    <img src={this.props.url} alt={this.props.teamName} onClick={this.handleClick}/>
+		</div>})
+  }
+
+  unhighlightImage(){
+    this.setState({image: 
+  		<div className = "TeamPicture">
+		    <img src={this.props.url} alt={this.props.teamName} onClick={this.handleClick}/>
+		</div>})  	
   }
 
   showAnimation(){
@@ -24,6 +48,7 @@ export default class Team extends Component {
   handleClick(){
   	if (this.state.numberOfClicks == 0){
   		this.showAnimation()
+  		this.highlightImage()
   		var that = this;
 		var username = "adeshpande3"
 		var password = "sportsfeedapi"
@@ -44,6 +69,7 @@ export default class Team extends Component {
   		this.setState({text: this.props.teamName})
   		this.setState({numberOfClicks: 1})
   	} else{
+  		this.unhighlightImage()
   		this.setState({text: ''})
   		this.setState({numberOfClicks: 0})
   		this.setState({teamData: ''})
@@ -53,9 +79,7 @@ export default class Team extends Component {
   render() {
     return (
     	<div>
-	    	<div className = "TeamPicture">
-		    	<img src={this.props.url} alt={this.props.teamName} onClick={this.handleClick}/>
-		    </div>
+	    	{this.state.image}
 		    {this.state.animation}
 	    	<div className = "TeamStats">
 		    	{this.state.teamData}
